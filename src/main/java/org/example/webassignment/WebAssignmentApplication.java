2037e891dc6f1c98a74a0434f8bd3e5fcd2e1dbf
Mansi <mansezimba14@gmail.com>
2024-07-19 14:46:47 +0545

    first commit

---
+++ src/main/java/org/example/webassignment/WebAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.webassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebAssignmentApplication.class, args);
+    }
+
+}
